b280d8edfa73558b378bec9e0b8799afe773ef79
Arman4ikS <arman.stepanyan.2017@bk.ru>
2024-05-10 23:28:52 +0300

    second commit

--- main.py
+++ main.py
@@ -1,16 +1,280 @@
-# This is a sample Python script.
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from pylab import rcParams
+import math
+import xgboost
+import time
+from tqdm import tqdm
+import keras.models
+import keras.layers
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+import warnings
+warnings.simplefilter('ignore')
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+data_train = pd.read_csv("/kaggle/input/nasa-cmaps/CMaps/train_FD001.txt",sep=" ",header=None)
+data_test = pd.read_csv("/kaggle/input/nasa-cmaps/CMaps/test_FD001.txt",sep=" ",header=None)
+data_RUL = pd.read_csv("/kaggle/input/nasa-cmaps/CMaps/RUL_FD001.txt",sep=" ",header=None)
+
+train_copy = data_train
+test_copy = data_test
+
+data_train
+
+data_test
+
+data_RUL
+
+data_train.drop(columns=[26,27],inplace=True)
+data_test.drop(columns=[26,27],inplace=True)
+data_RUL.drop(columns=[1],inplace=True)
+
+columns_train = ['unit_ID','cycles','setting_1','setting_2','setting_3','T2','T24','T30','T50','P2','P15','P30','Nf',
+           'Nc','epr','Ps30','phi','NRf','NRc','BPR','farB','htBleed','Nf_dmd','PCNfR_dmd','W31','W32' ]
+data_train.columns = columns_train
+data_train.describe()
+
+# Define a function to calculate the remaining useful life (RUL)
+def add_rul(g):
+    # Calculate the RUL as the difference between the maximum cycle value and the cycle value for each row
+    g['RUL'] = max(g['cycles']) - g['cycles']
+    return g
+
+# Apply the add_rul function to the training data grouped by the unit ID
+train = data_train.groupby('unit_ID').apply(add_rul)
+
+train.head()
+
+cnt_train = train[["unit_ID", "cycles"]].groupby("unit_ID").max().sort_values(by="cycles", ascending=False)
+cnt_ind = [str(i) for i in cnt_train.index.to_list()]
+cnt_val = list(cnt_train.cycles.values)
+
+plt.style.use("seaborn")
+plt.figure(figsize=(12, 30))
+sns.barplot(x=list(cnt_val), y=list(cnt_ind), palette='magma')
+plt.xlabel('Number of Cycles')
+plt.ylabel('Engine Id')
+plt.title('Number of Cycles for Engines', fontweight='bold', fontsize=24, pad=15)
+
+plt.show()
+
+plt.figure(figsize=(18, 9))
+subset_stats = data_train.agg(['mean', 'std']).T[2:]
+ax = sns.barplot(x=subset_stats.index, y="std", data=subset_stats, palette='magma')
+ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
+ax.set_xlabel("Sensor")
+ax.set_ylabel("Standard Deviation")
+ax.set_title("Standard Deviation of Each Column", fontweight='bold', fontsize=24, pad=15)
+
+for p in ax.patches:
+    ax.annotate(str(round(p.get_height(),3)), (p.get_x() * 1.005, p.get_height() * 1.005))
+
+plt.show()
+
+train.drop(columns=['Nf_dmd','PCNfR_dmd','P2','T2','setting_3','farB','epr'],inplace=True)
+
+sns.heatmap(train.corr(),annot=True,cmap='Blues',linewidths=0.2)
+fig=plt.gcf()
+fig.set_size_inches(20,20)
+plt.show()
+
+def process_targets(data_length, early_rul = None):
+    if early_rul == None:
+        return np.arange(data_length-1, -1, -1)
+    else:
+        early_rul_duration = data_length - early_rul
+        if early_rul_duration <= 0:
+            return np.arange(data_length-1, -1, -1)
+        else:
+            return np.append(early_rul*np.ones(shape = (early_rul_duration,)), np.arange(early_rul-1, -1, -1))
+
+def process_input_data_with_targets(input_data, target_data=None, window_length=1, shift=1):
+    num_batches = np.int(np.floor((len(input_data) - window_length) / shift)) + 1
+    num_features = input_data.shape[1]
+    output_data = np.repeat(np.nan, repeats=num_batches * window_length * num_features).reshape(num_batches,
+                                                                                                window_length,
+                                                                                                num_features)
+    if target_data is None:
+        for batch in range(num_batches):
+            output_data[batch, :, :] = input_data[(0 + shift * batch):(0 + shift * batch + window_length), :]
+        return output_data
+    else:
+        output_targets = np.repeat(np.nan, repeats=num_batches)
+        for batch in range(num_batches):
+            output_data[batch, :, :] = input_data[(0 + shift * batch):(0 + shift * batch + window_length), :]
+            output_targets[batch] = target_data[(shift * batch + (window_length - 1))]
+        return output_data, output_targets
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def process_test_data(test_data_for_an_engine, window_length, shift, num_test_windows=1):
+    max_num_test_batches = np.int(np.floor((len(test_data_for_an_engine) - window_length) / shift)) + 1
+    if max_num_test_batches < num_test_windows:
+        required_len = (max_num_test_batches - 1) * shift + window_length
+        batched_test_data_for_an_engine = process_input_data_with_targets(test_data_for_an_engine[-required_len:, :],
+                                                                          target_data=None,
+                                                                          window_length=window_length, shift=shift)
+        return batched_test_data_for_an_engine, max_num_test_batches
+    else:
+        required_len = (num_test_windows - 1) * shift + window_length
+        batched_test_data_for_an_engine = process_input_data_with_targets(test_data_for_an_engine[-required_len:, :],
+                                                                          target_data=None,
+                                                                          window_length=window_length, shift=shift)
+        return batched_test_data_for_an_engine, num_test_windows
 
+test_data = pd.read_csv("/kaggle/input/nasa-cmaps/CMaps/test_FD001.txt", sep = "\s+", header = None,names=columns_train )
+true_rul = pd.read_csv("/kaggle/input/nasa-cmaps/CMaps/RUL_FD001.txt", sep = '\s+', header = None)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+window_length = 30
+shift = 1
+early_rul = 125
+processed_train_data = []
+processed_train_targets = []
+num_test_windows = 5
+processed_test_data = []
+num_test_windows_list = []
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+columns_to_be_dropped =['unit_ID','setting_1','setting_2','setting_3', 'T2', 'P2','P15','P30', 'epr',
+                 'farB', 'Nf_dmd', 'PCNfR_dmd']
+
+train_data_first_column = data_train ["unit_ID"]
+test_data_first_column = test_data["unit_ID"]
+
+scaler = StandardScaler()
+train_data = scaler.fit_transform(data_train.drop(columns = columns_to_be_dropped))
+test_data = scaler.transform(test_data.drop(columns = columns_to_be_dropped))
+
+train_data = pd.DataFrame(data = np.c_[train_data_first_column, train_data])
+test_data = pd.DataFrame(data = np.c_[test_data_first_column, test_data])
+
+num_train_machines = len(train_data[0].unique())
+num_test_machines = len(test_data[0].unique())
+
+for i in np.arange(1, num_train_machines + 1):
+    temp_train_data = train_data[train_data[0] == i].drop(columns=[0]).values
+
+    # Determine whether it is possible to extract training data with the specified window length.
+    if (len(temp_train_data) < window_length):
+        print("Train engine {} doesn't have enough data for window_length of {}".format(i, window_length))
+        raise AssertionError("Window length is larger than number of data points for some engines. "
+                             "Try decreasing window length.")
+
+    temp_train_targets = process_targets(data_length=temp_train_data.shape[0], early_rul=early_rul)
+    data_for_a_machine, targets_for_a_machine = process_input_data_with_targets(temp_train_data, temp_train_targets,
+                                                                                window_length=window_length,
+                                                                                shift=shift)
+
+    processed_train_data.append(data_for_a_machine)
+    processed_train_targets.append(targets_for_a_machine)
+
+processed_train_data = np.concatenate(processed_train_data)
+processed_train_targets = np.concatenate(processed_train_targets)
+
+for i in np.arange(1, num_test_machines + 1):
+    temp_test_data = test_data[test_data[0] == i].drop(columns=[0]).values
+
+    # Determine whether it is possible to extract test data with the specified window length.
+    if (len(temp_test_data) < window_length):
+        print("Test engine {} doesn't have enough data for window_length of {}".format(i, window_length))
+        raise AssertionError("Window length is larger than number of data points for some engines. "
+                             "Try decreasing window length.")
+
+    # Prepare test data
+    test_data_for_an_engine, num_windows = process_test_data(temp_test_data, window_length=window_length, shift=shift,
+                                                             num_test_windows=num_test_windows)
+
+    processed_test_data.append(test_data_for_an_engine)
+    num_test_windows_list.append(num_windows)
+
+processed_test_data = np.concatenate(processed_test_data)
+true_rul = true_rul[0].values
+
+# Shuffle training data
+index = np.random.permutation(len(processed_train_targets))
+processed_train_data, processed_train_targets = processed_train_data[index], processed_train_targets[index]
+
+print("Processed trianing data shape: ", processed_train_data.shape)
+print("Processed training ruls shape: ", processed_train_targets.shape)
+print("Processed test data shape: ", processed_test_data.shape)
+print("True RUL shape: ", true_rul.shape)
+
+processed_train_data, processed_val_data, processed_train_targets, processed_val_targets = train_test_split(processed_train_data,
+                                                                                                            processed_train_targets,
+                                                                                                            test_size = 0.2,
+                                                                                                            random_state = 83)
+print("Processed train data shape: ", processed_train_data.shape)
+print("Processed validation data shape: ", processed_val_data.shape)
+print("Processed train targets shape: ", processed_train_targets.shape)
+print("Processed validation targets shape: ", processed_val_targets.shape)
+
+def create_compiled_model():
+    model = Sequential([
+        layers.LSTM(128, input_shape = (window_length, 14), return_sequences=True, activation = "tanh"),
+        layers.LSTM(64, activation = "tanh", return_sequences = True),
+        layers.LSTM(32, activation = "tanh"),
+        layers.Dense(96, activation = "relu"),
+        layers.Dense(128, activation = "relu"),
+        layers.Dense(1)
+    ])
+    model.compile(loss = "mse", optimizer = tf.keras.optimizers.Adam(learning_rate=0.001))
+    return model
+
+def scheduler(epoch):
+    if epoch < 5:
+        return 0.001
+    else:
+        return 0.0001
+callback = tf.keras.callbacks.LearningRateScheduler(scheduler, verbose = 1)
+model = create_compiled_model()
+history = model.fit(processed_train_data, processed_train_targets, epochs = 10,
+                    validation_data = (processed_val_data, processed_val_targets),
+                    callbacks = callback,
+                    batch_size = 128, verbose = 2)
+
+def scheduler(epoch):
+    if epoch < 5:
+        return 0.001
+    else:
+        return 0.0001
+callback = tf.keras.callbacks.LearningRateScheduler(scheduler, verbose = 1)
+model = create_compiled_model()
+history = model.fit(processed_train_data, processed_train_targets, epochs = 10,
+                    validation_data = (processed_val_data, processed_val_targets),
+                    callbacks = callback,
+                    batch_size = 128, verbose = 2)
+
+rul_pred = model.predict(processed_test_data).reshape(-1)
+preds_for_each_engine = np.split(rul_pred, np.cumsum(num_test_windows_list)[:-1])
+mean_pred_for_each_engine = [np.average(ruls_for_each_engine, weights = np.repeat(1/num_windows, num_windows))
+                             for ruls_for_each_engine, num_windows in zip(preds_for_each_engine, num_test_windows_list)]
+RMSE = np.sqrt(mean_squared_error(true_rul, mean_pred_for_each_engine))
+print("RMSE: ", RMSE)
+
+tf.keras.models.save_model(model, "FD001_LSTM_piecewise_RMSE_"+ str(np.round(RMSE, 4)) + ".h5")
+
+indices_of_last_examples = np.cumsum(num_test_windows_list) - 1
+preds_for_last_example = np.concatenate(preds_for_each_engine)[indices_of_last_examples]
+
+RMSE_new = np.sqrt(mean_squared_error(true_rul, preds_for_last_example))
+print("RMSE (Taking only last examples): ", RMSE_new)
+
+def compute_s_score(rul_true, rul_pred):
+    diff = rul_pred - rul_true
+    return np.sum(np.where(diff < 0, np.exp(-diff/13)-1, np.exp(diff/10)-1))
+s_score = compute_s_score(true_rul, preds_for_last_example)
+print("S-score: ", s_score)
+
+plt.plot(true_rul, label = "True RUL", color = "orange")
+plt.plot(preds_for_last_example, label = "Pred RUL", color = "blue")
+plt.legend()
+plt.show()
