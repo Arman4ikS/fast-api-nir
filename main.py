7db2ce4e158e5b0c4cdf71d5aa49f43658e567fd
Arman4ikS <arman.stepanyan.2017@bk.ru>
2024-06-08 15:47:23 +0300

    added sqlite3

--- main.py
+++ main.py
@@ -3,10 +3,33 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from second import processing
 import base64
+import sqlite3
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+def save_numbers_to_db(numbers, db_name='results.db'):
+    # Подключаемся к базе данных (если базы данных не существует, она будет создана)
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+
+    # Создаем таблицу, если она не существует
+    cursor.execute(f'''
+        CREATE TABLE IF NOT EXISTS results (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            number INTEGER NOT NULL
+        )
+    ''')
+
+    # Вставляем числа из массива в таблицу
+    cursor.executemany(f'''
+        INSERT INTO results (number) VALUES (?)
+    ''', [(number,) for number in numbers])
+
+    # Сохраняем изменения и закрываем соединение
+    conn.commit()
+    conn.close()
 
 @app.get("/", response_class=HTMLResponse)
 async def read_index():
@@ -40,6 +63,7 @@ async def upload_files(testData: UploadFile = File(...), trueRUL: UploadFile = F
     buffer_bytes, preds = processing(testData_contents, trueRUL_contents)
     base64_encoded_image = base64.b64encode(buffer_bytes.getvalue()).decode()
     preds = preds.tolist()
+    save_numbers_to_db(numbers, 'results.db')
     index_row = ''.join(f'<th>{i}</th>' for i in range(1, len(preds) + 1))
     data_row = ''.join(f'<td>{int(value)}</td>' for value in preds)
 
@@ -60,6 +84,37 @@ async def upload_files(testData: UploadFile = File(...), trueRUL: UploadFile = F
         </table>
     </body>
     </html>"""
+    return HTMLResponse(content=html_content2, status_code=200)
+
+def fetch_numbers_from_db(db_name='numbers.db', table_name='numbers_table'):
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+    cursor.execute(f'SELECT number FROM {table_name}')
+    numbers = cursor.fetchall()
+    conn.close()
+    return numbers
+@app.post("/results/")
+def generate_html(numbers = fetch_numbers_from_db(results.db)):
+    html_content = '''
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Numbers from Database</title>
+    </head>
+    <body>
+        <h1>Numbers</h1>
+        <table border="1">
+            <tr><th>ID</th><th>Number</th></tr>
+    '''
+
+    for idx, number in enumerate(numbers, start=1):
+        html_content += f'<tr><td>{idx}</td><td>{number[0]}</td></tr>\n'
+
+    html_content += '''
+        </table>
+    </body>
+    </html>
+    '''
     return HTMLResponse(content=html_content2, status_code=200)
 
 # Запуск сервера
